59a1f212a55d1281f49d17234486d0b818d2a1d0
DAN-KOTI\Daniel <daniel.blombergof@gmail.com>
2021-10-02 10:25:18 +0300

    added dashing.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,42 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float movementSpeed;
+    private float baseMovementSpeed;
+
+    //dashing variables
+    public float dashSpeed;
+    public float dashLength = 0.5f;
+    public float dashCooldown = 1f;
+    private float dashCounter = 0;
+    private float dashCoolCounter = 0;
 
 
+    private void Start() {
+        baseMovementSpeed = movementSpeed;
+    }
     // Update is called once per frame
     void Update()
-    {
+    {        
+        print(dashCoolCounter);
+        print(dashCounter);
+        //dashing
+        if(Input.GetKeyDown(KeyCode.Space)){
+            if(dashCoolCounter <= 0 && dashCounter <= 0){
+                movementSpeed = dashSpeed;
+                dashCounter = dashLength;
+            }
+        }
+        if(dashCounter > 0){
+            dashCounter -= Time.deltaTime;
+            if(dashCounter <= 0){
+                movementSpeed = baseMovementSpeed;
+                dashCoolCounter = dashCooldown;
+            }
+        }
+        if(dashCoolCounter > 0){
+            dashCoolCounter -= Time.deltaTime;
+        }
+
         Vector3 mov = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0).normalized * movementSpeed * Time.deltaTime;
         transform.Translate(mov);
     }
