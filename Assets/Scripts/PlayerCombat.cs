2e8499ca38a76322137b2f76a19080ae895bbdf9
tailow <tailows@outlook.com>
2021-10-03 15:40:43 +0300

    Sit on sword

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -10,7 +10,12 @@ public class PlayerCombat : MonoBehaviour
 
     public GameObject sword;
 
+    Vector2 hitDirection = Vector2.up;
+
+    public int damage;
     public float hitCooldown;
+    public float hitDistance;
+    public float hitAngle;
 
     float lastHit;
 
@@ -23,6 +28,11 @@ public class PlayerCombat : MonoBehaviour
 
     private void Update()
     {
+        if (playerMovement.movementDir != Vector2.zero)
+        {
+            hitDirection = playerMovement.movementDir;
+        }
+
         if (Input.GetMouseButtonDown(0) && Time.time - lastHit > hitCooldown)
         {
             Strike();
@@ -33,18 +43,28 @@ public class PlayerCombat : MonoBehaviour
 
     void Strike()
     {
-        Vector2 hitDirection = playerMovement.movementDir;
-
-        if (hitDirection == Vector2.zero)
-        {
-            hitDirection = Vector2.up;
-        }
-
         float angle = Mathf.Atan2(hitDirection.y, hitDirection.x) * Mathf.Rad2Deg;
         sword.transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
 
         sword.transform.localPosition = hitDirection;
 
         swordAnimator.SetTrigger("SwordHit");
+
+        foreach (Collider2D coll in Physics2D.OverlapCircleAll(transform.position, hitDistance))
+        {
+            if (!coll.CompareTag("Player"))
+            {
+                if (Vector2.Angle(hitDirection, coll.transform.position - transform.position) < hitAngle / 2)
+                {
+                    print("hit " + coll.name + " angle: " + Vector2.Angle(hitDirection, coll.transform.position - transform.position));
+
+                    if (coll.GetComponent<Health>() != null)
+                    {
+                        coll.GetComponent<Health>().TakeDamage(damage);
+                    }
+                }
+            }
+        }
+
     }
 }
